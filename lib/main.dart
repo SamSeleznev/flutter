9ec8a480ab31d37a4a224c0f0e465d426af284ae
SamSeleznev <92021434+SamSeleznev@users.noreply.github.com>
2023-05-16 10:42:58 +0600

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
+
 
 class MyApp extends StatefulWidget {
   @override
