2ddf77f26ab7bf55fdcf4b0cb1f471f83097055a
SamSeleznev <92021434+SamSeleznev@users.noreply.github.com>
2023-05-18 13:46:18 +0600

    version dart sdk

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:firebase_core/firebase_core.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main()  {
   runApp(MyApp());
 }
 
